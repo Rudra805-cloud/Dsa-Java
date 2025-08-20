1c9ed453a07e9d60faa1dbc632e7074d6dfbe7e3
Rudra805 <rudramishra9949@gmail.com>
2025-08-20 22:36:32 +0530

    hashing using hashmap

---
+++ Lecture-6/HashingusingMap.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class HashingusingMap {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int arr[]=new int[n];
+        for(int i=0;i<=n-1;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        HashMap<Integer,Integer>map=new HashMap<>();
+
+        //pre-compute
+        for(int i=0;i<=n-1;i++){
+         map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);  
+            // map[arr[i]]++;  =>in cpp
+        
+        }
+          
+
+
+        int q=sc.nextInt();
+        while (q>0) {
+            int num=sc.nextInt();
+            System.out.println(map.get(num));
+            
+        }
+
+
+    }
+}
